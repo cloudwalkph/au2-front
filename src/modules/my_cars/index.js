2de1c71b01d51b4c85077463440948fd10619cce
micinfo <michael.alumno@gmail.com>
2017-06-19 10:45:54 +0800

    my car list

---
+++ src/modules/my_cars/index.js
@@ -0,0 +1,102 @@
+import React, {Component} from 'react';
+
+class my_cars extends Component {
+
+	render() {
+		return(
+			<div className="mdl-grid fixed-width">
+				<div className="mdl-cell--1-offset mdl-cell mdl-cell--10-col">
+					<div className="mdl-grid">
+						<div className="mdl-cell mdl-cell--3-col">
+							<div className="demo-card-wide mdl-card mdl-shadow--2dp" style={{width: '100%'}}>
+								<div className="mdl-card__title" style={{background: 'url(images/USC60TOS121A021001.jpg) center / cover', minHeight: '160px'}}>
+									<h2 className="mdl-card__title-text">Welcome</h2>
+								</div>
+								<div className="mdl-card__supporting-text">
+									<p>Toyota Land Cruiser 2017 VX A'T</p>
+									<p>&#8369; 5,800.00</p>
+								</div>
+								<div className="mdl-card__actions mdl-card--border">
+									<a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+										<i className="material-icons">phone</i>
+									</a>
+								</div>
+								<div className="mdl-card__menu">
+									<button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+										<i className="material-icons">share</i>
+									</button>
+								</div>
+							</div>
+						</div>
+						<div className="mdl-cell mdl-cell--3-col">
+							<div className="demo-card-wide mdl-card mdl-shadow--2dp" style={{width: '100%'}}>
+								<div className="mdl-card__title" style={{background: 'url(images/USC60TOS121A021001.jpg) center / cover', minHeight: '160px'}}>
+									<h2 className="mdl-card__title-text">Welcome</h2>
+								</div>
+								<div className="mdl-card__supporting-text">
+									<p>Toyota Land Cruiser 2017 VX A'T</p>
+									<p>&#8369; 5,800.00</p>
+								</div>
+								<div className="mdl-card__actions mdl-card--border">
+									<a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+										<i className="material-icons">phone</i>
+									</a>
+								</div>
+								<div className="mdl-card__menu">
+									<button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+										<i className="material-icons">share</i>
+									</button>
+								</div>
+							</div>
+						</div>
+						<div className="mdl-cell mdl-cell--3-col">
+							<div className="demo-card-wide mdl-card mdl-shadow--2dp" style={{width: '100%'}}>
+								<div className="mdl-card__title" style={{background: 'url(images/USC60TOS121A021001.jpg) center / cover', minHeight: '160px'}}>
+									<h2 className="mdl-card__title-text">Welcome</h2>
+								</div>
+								<div className="mdl-card__supporting-text">
+									<p>Toyota Land Cruiser 2017 VX A'T</p>
+									<p>&#8369; 5,800.00</p>
+								</div>
+								<div className="mdl-card__actions mdl-card--border">
+									<a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+										<i className="material-icons">phone</i>
+									</a>
+								</div>
+								<div className="mdl-card__menu">
+									<button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+										<i className="material-icons">share</i>
+									</button>
+								</div>
+							</div>
+						</div>
+						<div className="mdl-cell mdl-cell--3-col">
+							<div className="demo-card-wide mdl-card mdl-shadow--2dp" style={{width: '100%'}}>
+								<div className="mdl-card__title" style={{background: 'url(images/USC60TOS121A021001.jpg) center / cover', minHeight: '160px'}}>
+									<h2 className="mdl-card__title-text">Welcome</h2>
+								</div>
+								<div className="mdl-card__supporting-text">
+									<p>Toyota Land Cruiser 2017 VX A'T</p>
+									<p>&#8369; 5,800.00</p>
+								</div>
+								<div className="mdl-card__actions mdl-card--border">
+									<a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+										<i className="material-icons">phone</i>
+									</a>
+								</div>
+								<div className="mdl-card__menu">
+									<button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+										<i className="material-icons">share</i>
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+
+}
+
+export default my_cars;
--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,13 @@ import Au2 from './modules/Au2';
 import profile from './modules/profile';
 import Registration from './modules/registration';
 import LoginForm from './modules/login';
+import my_cars from './modules/my_cars';
 
 export default (
     <Route path="/" component={Au2}>
         <Route path="/profile" component={profile} />
         <Route path="/login" component={LoginForm} />
         <Route path="/registration" component={Registration} />
+        <Route path="/myCars" component={my_cars} />
     </Route>
 )
